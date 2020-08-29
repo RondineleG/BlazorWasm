9617d03fe2ad1b3f93fbe22307e4cda20075babd
Rondineleg <rondineleg@edu.univali.br>
2020-08-29 15:09:53 -0300

    Criando um model

---
+++ BlazorWasm/Models/Developers.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWasm.Models
+{
+    public class Developers
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome e obrigatorio!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "O campo Sobrenome e obrigatorio!")]
+        public string LastName { get; set; }
+        
+        [Required(ErrorMessage = "O campo Email e obrigatorio!")]
+        public string Email { get; set; }
+        [Required]
+        public decimal Experience { get; set; } = 1;
+    }
+}
+
